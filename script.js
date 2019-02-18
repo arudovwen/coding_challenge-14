2437dd3eac6b7da8e8a38e3e6d4228549624a9c0
Arudovwen <successahon@gmail.com>
2019-02-18 18:14:52 +0100

    new updates

--- script.js
+++ script.js
@@ -1,54 +1,98 @@
 const cardsArray = [{
-        name: '1',
-        image: 'images/1.png'
-    },
-    {
-        name: '2',
-        image: 'images/7.png'
-    },
-    {
-        name: '3',
-        image: 'images/8.png'
-    },
-    {
-        name: '4',
-        image: 'images/9.png'
-    },
-    {
-        name: '5',
-        image: 'images/10.png'
-    },
-    {
-        name: '6',
-        image: 'images/11.png'
-    },
-    {
-        name: '7',
-        image: 'images/12.png'
-    },
-    {
-        name: '8',
-        image: 'images/13.png'
-    },
+    name: '1',
+    image: 'images/1.png'
+},
+{
+    name: '2',
+    image: 'images/7.png'
+},
+{
+    name: '3',
+    image: 'images/8.png'
+},
+{
+    name: '4',
+    image: 'images/9.png'
+},
+{
+    name: '5',
+    image: 'images/10.png'
+},
+{
+    name: '6',
+    image: 'images/11.png'
+},
+{
+    name: '7',
+    image: 'images/12.png'
+},
+{
+    name: '8',
+    image: 'images/13.png'
+},
 ]
 let firstGuess = '';
 let secondGuess = '';
 let count = 0;
 let prevClick = null;
-let delay = 1200;
+let delay = 1000;
 let movesCount = 0;
+let timer = {
+    sec: 0,
+    min: 0,
+    clearTime: -1
+}
+let interval;
 
 
 
 
-// move list
+// setting timer
+const timeCount = document.getElementById('timer');
+timeCount.innerHTML = `Timer: ${timer.min} mins ${timer.sec} secs`;
+const startTimer = () => {
+
+    interval = setInterval(function () {
+        timeCount.innerHTML = `Timer: ${timer.min} mins ${timer.sec} secs`;
+        timer.sec++
+        if (timer.sec === 60) {
+            timer.min++;
+            timer.sec = 0;
+        }
+        if (timer.min === 60) {
+            hour++;
+            timer.min = 0;
+        }
+
+    }, 1000);
+}
+
+function resetTimer() {
+    clearInterval(interval);
+    timer.sec = 0;
+    timer.min = 0;
+    timeCount.innerHTML = `Timer: ${timer.min} mins ${timer.sec} secs`;
+
+
+}
+
+//number of move list
 const moves = document.querySelector('#moves');
 const moveList = () => {
     movesCount++;
     moves.innerHTML = `Your Moves: ${movesCount}`;
 }
-// default number of moves
+// default number of moves made
 moves.innerHTML = `Your Moves: ${movesCount}`;
+
+
+
+const startGame = document.getElementById('start-game');
+startGame.addEventListener('click', () => {
+    
+    startTimer();
+})
+
 
 
 // reset entire game
@@ -58,7 +102,9 @@ resetGame.addEventListener('click', () => {
     secondGuess = '';
     count = 0;
     movesCount = 0;
+
     moves.innerHTML = `Your Moves: ${movesCount}`;
+
     let match = document.querySelectorAll('.match');
     match.forEach(card => {
         card.classList.remove('match');
@@ -67,7 +113,7 @@ resetGame.addEventListener('click', () => {
     selected.forEach(card => {
         card.classList.remove('is-flipped');
     })
-
+    resetTimer();
 })
 
 //to match cards
@@ -78,7 +124,7 @@ const match = () => {
     })
 }
 
-// reset card if they dont match
+// reset clicked cards if they dont match
 const reset = () => {
     firstGuess = '';
     secondGuess = '';
@@ -98,12 +144,14 @@ game.appendChild(grid);
 
 // double cards
 doubleCards = cardsArray.concat(cardsArray)
+// shuffle cards
 doubleCards.sort(() => 0.5 - Math.random());
 
 
 doubleCards.forEach(element => {
     const card = document.createElement('div');
     card.classList.add('card');
+    console.log(card);
 
     cardFront = document.createElement('div');
     cardFront.classList.add('card-face', 'card-front');
@@ -121,6 +169,9 @@ doubleCards.forEach(element => {
 
 
     // when card is clicked
+    if (!startGame ){
+        return;
+    }
     card.addEventListener('click', (event) => {
         const clicked = event.target;
 
@@ -159,7 +210,7 @@ doubleCards.forEach(element => {
             }
 
         }
-        prevClick = clicked;
+
     })
 
 
